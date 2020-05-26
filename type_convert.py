4757b64a9a7804358567b539e4bbd05e534559a8
Aly-Tomato <atamayo@pdx.edu>
2020-05-26 23:08:19 +0000

    handle errors when dealing with dirty data

--- type_convert.py
+++ type_convert.py
@@ -19,12 +19,21 @@ logging.basicConfig(filename='log_type_convert.log',
 logging.info("Begin Script")
 
 # convert all landmass and population values to int
-for state_obj in db.states.find({}, {"sightings":0, "_id":0}):
+state_objects = db.states.find({}, {"sightings":0})
+for state_obj in state_objects:
     population = state_obj.get("population")
-    int_pop = int(population.replace(",","")
+    try:
+        int_pop = population.replace(",","")
+    except:
+        int_pop = population
+    int_pop = int(int_pop)
 
     landmass = state_obj.get("landmass")
-    int_landmass = int(landmass.replace(",",""))
+    try:
+        int_landmass = landmass.replace(",","")
+    except:
+        int_landmass = landmass
+    int_landmass = int(int_landmass)
 
     state = state_obj.get("state")
 
@@ -32,12 +41,17 @@ for state_obj in db.states.find({}, {"sightings":0, "_id":0}):
     db.states.update({"state": state}, {"$set": {"population": int_pop, "landmass":int_landmass}}) 
 
 # convert all sightings datetime string to JSON
-for sighting in db.sightings.find():
-    og_datetime = sightings.get("datetime")
+all_sightings = db.sightings.find()
+for sighting in all_sightings:
+    og_datetime = sighting.get("datetime")
     strfmt = "%m/%d/%Y %H:%M"
-    new_datetime = datetime.strptime(og_datetime, strfmt)
+    try:
+        # Error occurs when dealing with a sighting from Rome that had an hour value of 936048
+        new_datetime = datetime.strptime(og_datetime, strfmt)
+    except:
+        new_datetime = None
 
     logging.info("replacing {} with {}".format(population, int_pop))
-    db.sightings.update({"_id": sightings.get("_id")}, {"$set": {"datetime": new_datetime}}) 
+    db.sightings.update({"_id": sighting.get("_id")}, {"$set": {"datetime": new_datetime}}) 
 
 logging.info("success")
